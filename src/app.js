89bd4784a024c1156a28c091ff32ad6d8e0e2e01
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-04-11 19:15:27 +0530

    healtcheck

--- src/app.js
+++ src/app.js
@@ -16,4 +16,15 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 
+//import routes
+import healthcheckrouter from "./routes/healthcheck.routes.js"
+import { healthcheck } from "./controllers/healthcheck.controller.js"
+
+
+//routes
+app.use("/api/v1/healthcheck",healthcheckrouter)
+
+
+
+
 export {app}
