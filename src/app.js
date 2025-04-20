ac2cb34140ba193c7072798749859f215ed73084
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-04-20 16:08:58 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-
+import cookieParser from "cookie-parser"
 
 const app=express()
 
@@ -15,7 +15,7 @@ app.use(
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
-
+app.use(cookieParser())
 //import routes
 import healthcheckrouter from "./routes/healthcheck.routes.js"
 import { healthcheck } from "./controllers/healthcheck.controller.js"
