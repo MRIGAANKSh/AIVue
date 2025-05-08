6714a63863cc75244ed79172658bc69141490e2c
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-05-08 19:00:07 +0530

    done

---
+++ src/controllers/dashboard.controllers.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import {Video} from "../models/video.models.js"
+import {Subscription} from "../models/subscription.models.js"
+import {Like} from "../models/like.models.js"
+import {ApiError} from "../utils/apierror.js"
+import {ApiResponse} from "../utils/apiresponse.js"
+import {asyncHandler} from "../utils/asynchandler.js"
+
+const getChannelStats = asyncHandler(async (req, res) => {
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+})
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+    // TODO: Get all the videos uploaded by the channel
+})
+
+export {
+    getChannelStats, 
+    getChannelVideos
+    }
